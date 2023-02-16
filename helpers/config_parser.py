e9f4bb846d02d74c41a5ab02bacddd45a46a1ca6
Maria Koszucka <koszucka.maria@gmail.com>
2023-02-16 16:08:34 +0100

    chagnes

--- helpers/config_parser.py
+++ helpers/config_parser.py
@@ -1,6 +1,7 @@
 from configparser import ConfigParser, ExtendedInterpolation
 from os import path, getcwd
-
+from helpers.logger import app_logger
 config_parser = ConfigParser(interpolation=ExtendedInterpolation())
+app_logger.error(f"Get current dir: {getcwd()}")
 config_file = path.join(getcwd(), "config.ini")
 config_parser.read_file(open(config_file, encoding='utf-8'))
